0f13dbdbec195adc590f8060eef13977ee33b2a1
Luke De Mouy <lukedemouy@gmail.com>
2021-03-03 13:18:20 -0700

    Convert Numbers to Tokens as a special case for lexing

--- nail_lexer/src/lib.rs
+++ nail_lexer/src/lib.rs
@@ -19,10 +19,22 @@ impl Lexer {
     /// Takes a line of text, presumably from direct textual user input
     /// and turns it into a vector of tokens based upon the grammar that
     /// was passed into the new function. Tokens are split on spaces.
+    /// Numbers are automatically handled, for purposes of the lexer
+    /// no distinction is made between floats and integers.
     pub fn lex(&self, line: &str) -> Vec<nail_common::Token> {
         let lexemes: Vec<&str> = line.split(' ').collect();
         let mut tokens = vec![];
         for lexeme in lexemes {
+            //Special Case: Number
+            if lexeme.parse::<f64>().is_ok() {
+                tokens.push(nail_common::Token {
+                    variant: "number".to_string(),
+                    contents: lexeme.to_string(),
+                });
+                continue;
+            }
+
+            //Normal Case: In Grammar
             let mut translated = false;
             for gramm in &self.grammar {
                 if lexeme == gramm.name || gramm.synonyms.iter().any(|f| f == lexeme) {
@@ -33,6 +45,7 @@ impl Lexer {
                     translated = true;
                 }
             }
+            //Case Unknown: not in grammar
             if !translated {
                 tokens.push(nail_common::Token {
                     variant: "unknown".to_string(),
@@ -61,6 +74,30 @@ mod tests {
         assert_eq!(grammar.len(), 2);
 
         Ok(Lexer::new(grammar))
+    }
+
+    #[test]
+    fn lex_single_integer() -> Result<(), Error> {
+        let lexer = create_lexer()?;
+
+        let tokens = lexer.lex("1");
+        assert_eq!(tokens.len(), 1);
+        assert_eq!(tokens[0].variant, "number");
+        assert_eq!(tokens[0].contents, "1");
+
+        Ok(())
+    }
+
+    #[test]
+    fn lex_single_float() -> Result<(), Error> {
+        let lexer = create_lexer()?;
+
+        let tokens = lexer.lex("1.0");
+        assert_eq!(tokens.len(), 1);
+        assert_eq!(tokens[0].variant, "number");
+        assert_eq!(tokens[0].contents, "1.0");
+
+        Ok(())
     }
 
     #[test]
